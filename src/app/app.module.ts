a1a3f82c079f7fa38d850a611c57c076ebda97e4
SantaRenata <renatasanta2002@gmail.com>
2023-04-18 17:32:18 +0200

    komponens és szolgáltatás létrehozva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShipComponent } from './ship/ship.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShipComponent
   ],
   imports: [
     BrowserModule
